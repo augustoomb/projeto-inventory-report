5050fc61929737e534582967ac6b2b832eeb251d
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-12-15 18:40:40 -0300

    test: Testando o construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        10,
+        "Chocolate",
+        "Nestle",
+        "2022-10-10",
+        "2023-11-11",
+        "58489491",
+        "Local fresco",
+    )
+
+    assert type(product.id) is int
+    assert type(product.nome_do_produto) is str
+    assert type(product.nome_da_empresa) is str
+    assert type(product.data_de_fabricacao) is str
+    assert type(product.data_de_validade) is str
+    assert type(product.numero_de_serie) is str
+    assert type(product.instrucoes_de_armazenamento) is str
