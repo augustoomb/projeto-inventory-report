ffc10adcf0f416fdb97e24e26d0be4d77541420e
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-12-18 21:26:02 -0300

    feat: gerador de relatório completo enviando arquivo .json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,7 @@
 import csv
+import json
+import xml
+import os
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -6,21 +9,64 @@ from inventory_report.reports.complete_report import CompleteReport
 
 class Inventory:
     @classmethod
-    def import_data(cls, path, type_report):
+    def generate_list_with_data(cls, data):
+        list_dict = []
+
+        for item in data:
+            list_dict.append(item)
+
+        return list_dict
+
+    @classmethod
+    def get_data_in_csv(cls, path):
         with open(path, encoding="utf-8") as file:
-            content = csv.DictReader(file, delimiter=",", quotechar='"')
+            data = csv.DictReader(file, delimiter=",", quotechar='"')
+            return cls.generate_list_with_data(data)
 
-            list_dict = []
+    @classmethod
+    def get_data_in_json(cls, path):
+        with open(path) as file:
+            data = file.read()
+            return json.loads(data)
 
-            for item in content:
-                list_dict.append(item)
+    # @classmethod
+    # def get_data_in_xml(cls, path):
+    #     with open(path, encoding="utf-8") as file:
+    #         data = csv.DictReader(file, delimiter=",", quotechar='"')
+    #         return cls.generate_list_with_data(data)
 
-            if type_report == "simples":
-                return SimpleReport.generate(list_dict)
+    @classmethod
+    def get_extension(cls, path):
+        file_name, file_extension = os.path.splitext(path)
+        return file_extension
 
-            # if type_report == "completo":
-            else:
-                return CompleteReport.generate(list_dict)
+    @classmethod
+    def get_data(cls, path):
+        file_extension = cls.get_extension(path)
+
+        if file_extension == ".csv":
+            data = cls.get_data_in_csv(path)
+            return data
+
+        if file_extension == ".json":
+            data = cls.get_data_in_json(path)
+            return data
+
+        if file_extension == ".xml":
+            data = cls.get_data_in_xml(path)
+            return data
+
+    @classmethod
+    def import_data(cls, path, type_report):
+
+        content = cls.get_data(path)
+
+        if type_report == "simples":
+            return SimpleReport.generate(content)
+
+        # if type_report == "completo":
+        else:
+            return CompleteReport.generate(content)
 
 
-# Inventory.import_data("inventory_report/data/inventory.csv", "simples")
+# Inventory.import_data("inventory_report/data/inventory.json", "simples")
